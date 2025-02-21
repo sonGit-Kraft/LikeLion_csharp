f51dc9580cdcdbad6097a9c56ba9d04230ac528b
sonGit-Kraft <sdhunituser@gmail.com>
2025-02-21 11:08:32 +0900

    Study4

---
+++ LikeLion4/LikeLion4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting; // 문자열 변수 선언
+            greeting = "Hello, World!"; // 변수에 값 저장
+
+            Console.WriteLine(greeting); // 출력: Hello, World!
+        }
+    }
+}
