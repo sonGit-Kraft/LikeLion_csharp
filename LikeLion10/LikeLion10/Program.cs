3ea02b06d7432d59fbd680e1de090151e29f668c
sonGit-Kraft <sdhunituser@gmail.com>
2025-02-21 16:02:04 +0900

    Study

---
+++ LikeLion10/LikeLion10/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float ruin;
+            float card;
+            float awakening;
+            int manaMax;
+            int manaHF;
+            int manaHNF;
+            float speed_m;
+            float speed_v;
+            float speed_c;
+            float skill;
+
+            Console.WriteLine("<활동 내역 입력>");
+            Console.Write("루인 스킬 피해(%): ");
+            ruin = float.Parse(Console.ReadLine());
+            Console.Write("카드 게이지 획득량(%): ");
+            card = float.Parse(Console.ReadLine());
+            Console.Write("각성기 피해(%): ");
+            awakening = float.Parse(Console.ReadLine());
+            Console.Write("최대 마나: ");
+            manaMax = int.Parse(Console.ReadLine());
+            Console.Write("전투 중 마나 회복량: ");
+            manaHF = int.Parse(Console.ReadLine());
+            Console.Write("비전투 중 마나 회복량: ");
+            manaHNF = int.Parse(Console.ReadLine());
+            Console.Write("이동 속도(%): ");
+            speed_m = float.Parse(Console.ReadLine());
+            Console.Write("탈 것 속도(%): ");
+            speed_v = float.Parse(Console.ReadLine());
+            Console.Write("운반 속도(%): ");
+            speed_c = float.Parse(Console.ReadLine());
+            Console.Write("스킬 재사용 대기시간 감소(%): ");
+            skill = float.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("<활동 내역 출력>");
+            Console.WriteLine("루인 스킬 피해: " + ruin + '%');
+            Console.WriteLine("카드 게이지 획득량: " + card + '%');
+            Console.WriteLine("각성기 피해: " + awakening + '%');
+            Console.WriteLine("최대 마나: " + manaMax);
+            Console.WriteLine("전투 중 마나 회복량: " + manaHF);
+            Console.WriteLine("비전투 중 마나 회복량: " + manaHNF);
+            Console.WriteLine("이동 속도: " + speed_m + '%');
+            Console.WriteLine("탈 것 속도: " + speed_v + '%');
+            Console.WriteLine("운반 속도: " + speed_c + '%');
+            Console.WriteLine("스킬 재사용 대기시간 감소: " + skill + '%');
+        }
+    }
+}
---
+++ LikeLion9/LikeLion9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 사용자 입력을 문자열로 받기
+            Console.Write("이름을 입력하세요: ");
+            string userName = Console.ReadLine(); // 사용자로부터 입력 받기
+            
+            Console.WriteLine($"안녕하세요, {userName}님!"); // 입력값 출력
+
+            // 문자열을 정수로 변환
+            // Console.ReadLine()은 항상 문자열로 입력을 받으므로
+            // 숫자 등 다른 데이터 형식으로 사용할 경우 변환이 필요
+            Console.Write("나이를 입력하세요: ");
+            string input = Console.ReadLine(); // 입력
+            int age = int.Parse(input); // string -> int (문자열을 정수로 변환)
+
+            Console.WriteLine($"내년에는 {age + 1}살이 되겠군요!");
+            Console.WriteLine("올해는 " + age + "살이군요!");
+            Console.WriteLine("올해는 {0}살이군요!", age);
+        }
+    }
+}
