7b6a9cb5306161084b00850f5030c2627131496a
sonGit-Kraft <sdhunituser@gmail.com>
2025-02-21 11:05:05 +0900

    study

--- LikeLion3/LikeLion3/Properties/AssemblyInfo.cs
+++ LikeLion3/LikeLion3/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
-[assembly: AssemblyTitle("Study3")]
+[assembly: AssemblyTitle("LikeLion3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Study3")]
+[assembly: AssemblyProduct("LikeLion3")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("176bef43-a69f-45e9-9880-e32e5229ff64")]
+[assembly: Guid("96cf9cd8-ec5d-4d09-9711-101acaa3295a")]
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
 //
