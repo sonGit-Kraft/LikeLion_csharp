ae8f162edb8518f6f5677be1954774bb9454c12f
sonGit-Kraft <sdhunituser@gmail.com>
2025-02-21 10:43:16 +0900

    Mini Project

---
+++ Study3/Study3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hp = 100; // 정수형 리터럴
+            double att = 56.7; // 실수형 리터럴
+            string name = "???"; // 문자열 리터럴
+            char grade = 'S'; // 문자형 리터럴
+
+            // 출력
+            Console.WriteLine(hp);
+            Console.WriteLine(att);
+            Console.WriteLine(name);
+            Console.WriteLine(grade);
+        }
+    }
+}
