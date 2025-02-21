a7c3609077b8bd73a959b510d734354e3a57620b
sonGit-Kraft <sdhunituser@gmail.com>
2025-02-21 15:04:37 +0900

    Study

---
+++ Loading/Loading/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Loading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("□□□□□□□□□□");
+            Thread.Sleep(1000); // 1초 딜레이 (1000 밀리초)
+            Console.Clear(); // 콘솔 화면 지우기
+            Console.WriteLine("■□□□□□□□□□");
+            Thread.Sleep(1000); // 1초 딜레이 (1000 밀리초)
+            Console.Clear(); // 콘솔 화면 지우기
+            Console.WriteLine("■■□□□□□□□□");
+            Thread.Sleep(1000); // 1초 딜레이 (1000 밀리초)
+            Console.Clear(); // 콘솔 화면 지우기
+            Console.WriteLine("■■■□□□□□□□");
+            Thread.Sleep(1000); // 1초 딜레이 (1000 밀리초)
+            Console.Clear(); // 콘솔 화면 지우기
+            Console.WriteLine("■■■■□□□□□□");
+            Thread.Sleep(1000); // 1초 딜레이 (1000 밀리초)
+            Console.Clear(); // 콘솔 화면 지우기
+            Console.WriteLine("■■■■■□□□□□");
+            Thread.Sleep(1000); // 1초 딜레이 (1000 밀리초)
+            Console.Clear(); // 콘솔 화면 지우기
+            Console.WriteLine("■■■■■■□□□□");
+            Thread.Sleep(1000); // 1초 딜레이 (1000 밀리초)
+            Console.Clear(); // 콘솔 화면 지우기
+            Console.WriteLine("■■■■■■■□□□");
+            Thread.Sleep(1000); // 1초 딜레이 (1000 밀리초)
+            Console.Clear(); // 콘솔 화면 지우기
+            Console.WriteLine("■■■■■■■■□□");
+            Thread.Sleep(1000); // 1초 딜레이 (1000 밀리초)
+            Console.Clear(); // 콘솔 화면 지우기
+            Console.WriteLine("■■■■■■■■■□");
+            Thread.Sleep(1000); // 1초 딜레이 (1000 밀리초)
+            Console.Clear(); // 콘솔 화면 지우기
+            Console.WriteLine("■■■■■■■■■■");
+        }
+    }
+}
