8f08967ba580ff52e3257e08b4e8793daefa429f
sonGit-Kraft <sdhunituser@gmail.com>
2025-02-21 10:44:21 +0900

    Study2

--- LikeLion2/LikeLion2/Program.cs
+++ LikeLion2/LikeLion2/Program.cs
@@ -13,7 +13,7 @@ namespace LikeLion2
             int age; // 정수형 변수 선언
             age = 25; // 변수 값 저장
 
-            Console.WriteLine(age); // 변수에 저장된 갑 출력
+            Console.WriteLine(age); // 변수에 저장된 값 출력
         }
     }
 }
