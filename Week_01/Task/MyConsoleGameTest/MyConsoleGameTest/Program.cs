9007806c596a6d462e15570924c4c77d21d78cc8
sonGit-Kraft <sdhunituser@gmail.com>
2025-03-02 21:34:38 +0900

    Study

--- Week_01/Task/MyConsoleGameTest/MyConsoleGameTest/Program.cs
+++ Week_01/Task/MyConsoleGameTest/MyConsoleGameTest/Program.cs
@@ -33,11 +33,14 @@ namespace ConsoleApp1
 
             for (int i = 0; i < mapData.Length; i++)
                 Console.WriteLine($"{i} + {mapData[i]}");
+
             int width = 50;
             int x = 4, y = 2;
             int index = y * (width + 2) + x; // 개행 포함
+
             mapData.Remove(index, 2);       // 두 칸 삭제
             mapData.Insert(index, "  ");    // 공백 두 칸 삽입
+
             for (int i = 0; i < mapData.Length; i++)
                 Console.Write($"{mapData[i]}");
 
