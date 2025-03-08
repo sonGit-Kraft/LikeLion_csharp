1e41242c616413b666788313aefa39b34fee1c89
sonGit-Kraft <sdhunituser@gmail.com>
2025-03-08 19:49:08 +0900

    Study

---
+++ Week_02/Task/TextRPGTest/TextRPGTest/Monster.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPGTest
+{
+    class Monster : Unit
+    {
+        public void ShowInfo()
+        {
+            Console.WriteLine("==========================");
+            Console.WriteLine("몬스터 이름: " + Name);
+            Console.WriteLine("체력: " + Hp + "\t공격력: " + Attack);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Week_02/Task/TextRPGTest/TextRPGTest/Unit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPGTest
+{
+    class Unit
+    {
+        public string Name;
+        public int Attack;
+        public int Hp;
+
+        public void GetDamage(Unit target)
+        {
+            Hp -= target.Attack;
+        }
+    }
+}
